d705dea5ed88891dc56e0440559d03a30dc636a4
Techhadera <rdahad>
2021-01-24 18:01:04 +0300

    feat: almost done task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if ((!Array.isArray(arr))) {
+    throw new Error();
+  }
+  
+  let transformedArr = [];
+  for(let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-next') {
+      i++;
+    }
+    else if (arr[i] === '--discard-prev') {
+      transformedArr.pop();
+    }
+    else if (arr[i] === '--double-next') {
+      if (i != arr.length - 1) {
+        transformedArr.push(arr[i + 1]);
+      } 
+    }
+    else if (arr[i] === '--double-prev') {
+      if (i != 0) {
+        transformedArr.push(arr[i - 1]);
+      }
+    } else {
+      transformedArr.push(arr[i]);
+    }
+  }
+  
+  return transformedArr;
 };
